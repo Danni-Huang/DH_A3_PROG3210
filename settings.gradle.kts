3707994c7a43d2b47cb03506a9296180c19d399f
Danni-Huang <dhuang.ni@gmail.com>
2023-11-30 20:57:55 -0500

    Navigation

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DH_A3_PROG3210"
+include(":app")
+ 
